efa0d5d91bc458004e81ad4675ead9353976c49f
Franco <francofl99@gmail.com>
2020-11-20 14:50:13 -0300

    fixed grid of contacts cards

--- resources/js/components/CardContainer.vue
+++ resources/js/components/CardContainer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="cards-view grid grid-cols-3 gap-3 py-2 px-2 items-center place-items-center bg-gray-400 rounded-md">
-        <contact-card v-for="contact in contacts" class="col-span-1"
+  <div class="cards-view grid-rows-2 grid grid-cols-3 py-4 px-2 items-center place-items-center bg-gray-400 rounded-md">
+        <contact-card v-for="contact in contacts"
             :key="contact.id"
             :contactId="contact.id"
             :contactName="contact.nombre"
